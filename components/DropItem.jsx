9cac864456c04a3466a6ad128b953e579e96ebcd
kreyson999 <mlynekjakub05@gmail.com>
2022-04-21 22:22:29 +0200

    started building desktop homepage

--- components/DropItem.jsx
+++ components/DropItem.jsx
@@ -8,8 +8,8 @@ import Price from "./Price";
 function DropItem({ name, slug, featuredImage, resell, retail }) {
   return (
     <Link href={`/drop/${slug}`}>
-      <a className="border-2 border-light-blue p-1.5 flex">
-        <div className="grid max-w-[128px] w-full">
+      <a className="border-2 border-light-blue p-1.5 flex space-x-2">
+        <div className="grid col-span-5 h-[128px] w-[128px] md:h-[175px] md:w-[175px] xl:h-[200px] xl:w-[200px]">
           <Image
             src={featuredImage.url}
             width={200}
@@ -19,10 +19,10 @@ function DropItem({ name, slug, featuredImage, resell, retail }) {
           />
         </div>
         <div className="grow flex flex-col text-right items-end justify-between">
-          <span className="font-bold uppercase text-light-blue text-lg">
+          <span className="font-bold uppercase text-light-blue text-lg lg:text-xl">
             {name}
           </span>
-          <div className="flex flex-col items-end">
+          <div className="flex flex-col items-end md:text-lg">
             <Price title="Retail" price={retail} />
             <Price title="Resell" price={resell} />
           </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -22,7 +22,7 @@ export default function Home({ hotDrops, drops }) {
     <>
       <SeoHead {...seoObject} />
       <header className="before:absolute before:w-full before:h-1/3 before:bg-blue before:left-0 before:top-0">
-        <nav className="relative z-10 flex items-center justify-between px-4 pt-4">
+        <nav className="max-w-screen-xl mx-auto relative z-10 flex items-center justify-between px-4 pt-4">
           <div className="flex items-center">
             <div className="grid w-12">
               <Image
@@ -43,8 +43,8 @@ export default function Home({ hotDrops, drops }) {
             />
           </button>
         </nav>
-        <div className="flex flex-col">
-          <div className="relative z-10 grid mx-4 my-4 px-1.5 py-1.5 border-2 border-light-blue">
+        <div className="max-w-screen-xl mx-auto flex flex-col">
+          <div className="relative max-w-[500px] self-center mx-4 z-10 grid my-4 px-1.5 py-1.5 border-2 border-light-blue">
             <Image
               src="/images/but.png"
               width={500}
@@ -53,10 +53,10 @@ export default function Home({ hotDrops, drops }) {
             />
           </div>
           <div className="text-right mx-4">
-            <h2 className="uppercase font-extrabold text-3xl text-light-blue">
+            <h2 className="uppercase font-extrabold text-[2.1rem] leading-[2.25rem] text-light-blue">
               Air Jordan 2 x union grey fog
             </h2>
-            <span className="text-lg font-extralight">
+            <span className="text-xl font-extralight">
               {getFormattedDate(hotDrops[0].date)}
             </span>
           </div>
@@ -83,13 +83,13 @@ export default function Home({ hotDrops, drops }) {
           <hr className="grow border-t" />
         </div>
       </header>
-      <section className="px-4 mt-6">
+      <section className="px-4 pt-6 pb-8 max-w-screen-xl mx-auto">
         <h3 className="text-xl text-light-blue font-extralight uppercase mb-4">
           Nadchodzące dropy:
         </h3>
-        <div className="grid gap-3">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
           {drops.map((drop) => (
-            <DropItem {...drop} />
+            <DropItem key={drop.name} {...drop} />
           ))}
         </div>
       </section>
